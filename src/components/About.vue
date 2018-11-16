5c9bae513ecf5a5c87afb86599e0e6e4ca85f64f
Brandon <brandonbevans@gmail.com>
2018-11-15 17:22:07 -0800

    reorg

---
+++ src/components/About.vue
@@ -0,0 +1,32 @@
+<template>
+    <v-layout align-center column justify-center id="about">
+        <v-card flat width="50%">
+            <v-container align-center md3>
+                <v-layout align-center justify-center>
+                    <v-flex xs12 flexbox>
+                        <span v-text=bakingDescription ></span>
+                    </v-flex>
+                </v-layout>
+            </v-container>
+        </v-card>
+    </v-layout>
+</template>
+
+<script>
+    export default {
+        name: "About",
+        data: function() {
+            return {
+                bakingDescription: "We're a group of Tezos fanatics who will continue to develop products on the" +
+                    " Tezos network. By delegating to us, you're joining a team that will scale with the growth of" +
+                    " the Tezos ecosystem. We will continue to grow and participate with the network as it matures." +
+                    " If you have project ideas, have questions about the delegation process, or want to reach out" +
+                    " for any other reason - please reach out."
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/Title.vue
@@ -0,0 +1,47 @@
+<template>
+    <v-layout
+            align-center
+            column
+            justify-center
+    >
+        <h1 class="display-4 font-weight-bold mb-3">Lucky Stakes</h1>
+        <v-card flat >
+            <v-container fill-height class="clickable"
+                         v-clipboard:copy=bakingAddress
+                         v-clipboard:success=onCopy>
+                <v-layout fill-height>
+                    <v-flex xs12 align-end flexbox>
+                        <span class="headline" v-text=bakingAddress></span>
+                    </v-flex>
+                </v-layout>
+            </v-container>
+        </v-card>
+        <a :href=tzscanLink target="_blank" class="text--darken-1 white--text">tzscan</a>
+        <div class="text-xs-center" :style="{ visibility: showCopy }">
+            <v-chip dark color="success" >Copied</v-chip>
+        </div>
+    </v-layout>
+</template>
+
+<script>
+    export default {
+        name: "Title",
+        data: function() {
+            return {
+                bakingAddress: "tz1iJ4qgGTzyhaYEzd1RnC6duEkLBd1nzexh",
+                tzscanLink: "https://tzscan.io/tz1iJ4qgGTzyhaYEzd1RnC6duEkLBd1nzexh",
+                showCopy: "hidden"
+            }
+        },
+        methods: {
+            onCopy: function () {
+                this.showCopy = "visible"
+                setTimeout(() => this.showCopy = "hidden", 600)
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
