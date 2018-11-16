a75be89031b4b520003349a81d63b09db41d2ab2
Brandon <brandonbevans@gmail.com>
2018-11-16 15:19:52 -0800

    https redirect

--- server.js
+++ server.js
@@ -3,9 +3,12 @@ var express = require('express');
 var serveStatic = require('serve-static');
 var secure = require('express-force-https');
 
+var httpsRedirect = require('express-https-redirect');
+
 app = express();
 app.use(serveStatic(__dirname + "/dist"));
 app.use(secure)
+app.use('/', httpsRedirect());
 var port = process.env.PORT || 5000;
 app.listen(port);
 console.log('server started '+ port);
