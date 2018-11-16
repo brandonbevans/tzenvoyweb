f9ea44bd353ce608e451dac9c3318c6ef740f1c9
Brandon <brandonbevans@gmail.com>
2018-11-16 15:12:26 -0800

    force https

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 // server.js
 var express = require('express');
-var path = require('path');
 var serveStatic = require('serve-static');
+var secure = require('express-force-https');
+
 app = express();
 app.use(serveStatic(__dirname + "/dist"));
+app.use(secure)
 var port = process.env.PORT || 5000;
 app.listen(port);
 console.log('server started '+ port);
