ea68a3cd86d9df7c429811605277a1bfa1b72948
Brandon Bevans <brandon.bevans@airbnb.com>
2019-03-24 17:14:28 -0700

    add townhall description

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -10,9 +10,10 @@
                         <v-img :src="require('@/assets/programming.jpeg')"></v-img>
                     </v-flex>
                     <v-flex xs6>
-                        <h3 class="headline">Projects</h3>
-                        <div>We're working on it!</div>
-                        <div>If you have project ideas and/or you're not afraid of OCaml, connect with us!</div>
+                        <h3 class="headline">Projects</h3><br><br>
+                        <h4 class="subheading"><b>TownHall</b></h4>
+                        <span v-html="townHallDescription"></span>
+                        <div> </div>
                     </v-flex>
                 </v-layout>
             </v-container>
@@ -22,7 +23,16 @@
 
 <script>
     export default {
-        name: "Projects"
+        name: "Projects",
+        data: () => ({
+            townHallDescription: "Now that Tezos' voting is in full swing, there needs to be a way to allow delegators" +
+                " to voice their opinion to their bakers. This is essential for a republic. TownHall aims to provide a " +
+                "forum for delegators to discuss and share their opinions of votes to their bakers and other delegators." +
+                " TownHall will turn baking into more than just a business model, now they will be able to meet their" +
+                " political responsibility that they owe to delegators.<br><br>" +
+                "TownHall will also be cryptographically protected. You will only be able to see the discussion of your baker's TownHall, " +
+                "and you'll use your Tezos wallet to prove which TownHall you belong to."
+        })
     }
 </script>
 
